34958ac86bf327a8611a28cb15192ad3b7989415
AngelBarreraC <420087896@pcpuma.acatlan.unam.mx>
2022-05-31 00:08:07 -0500

    colors

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Bienvenido Sistema de Inventario</h3></div>
                                     <div class="card-body">
                                         <form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>"> <!--Esto es para que el formulario se vuelva a cargar-->
                                             <div class="form-floating mb-3">
